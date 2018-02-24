6902001fe91c251a04c3406ad817a9b5ded21e58
haifeiWu <1253242444@qq.com>
2018-02-24 10:43:26 +0800

    [lightconf] add log and app interface.

---
+++ lightconf-admin/lightconf-admin-web/src/main/java/com/lightconf/admin/web/controller/AppController.java
@@ -0,0 +1,16 @@
+package com.lightconf.admin.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 应用管理接口.
+ *
+ * @author wuhf
+ * @date 2018/02/24
+ */
+@Controller
+@RequestMapping("/app")
+public class AppController extends BaseController {
+
+}
---
+++ lightconf-admin/lightconf-admin-web/src/main/java/com/lightconf/admin/web/controller/BaseController.java
@@ -0,0 +1,11 @@
+package com.lightconf.admin.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BaseController {
+    /**
+     * 日志对象
+     */
+    protected Logger LOGGER = LoggerFactory.getLogger(getClass());
+}
