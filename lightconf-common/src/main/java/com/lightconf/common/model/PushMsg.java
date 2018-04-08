6d3740088a19a92644b53b1e136c36321b36f352
haifeiWu <whfstudio@163.com>
2018-04-08 23:12:50 +0800

    update

---
+++ lightconf-common/src/main/java/com/lightconf/common/model/PushMsg.java
@@ -0,0 +1,31 @@
+package com.lightconf.common.model;
+
+import com.alibaba.fastjson.annotation.JSONType;
+
+/**
+ * 服务端推送消息实体。
+ * @author whfstudio@163.com
+ * @date 2018/04/08
+ */
+@JSONType(typeName = "loginMsg")
+public class PushMsg extends BaseMsg {
+
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
