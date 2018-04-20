f67cde377a71e41c5bf0563764681de4e201d5dd
haifeiWu <1253242444@qq.com>
2018-04-20 22:23:16 +0800

    add cross domain filter

---
+++ lightconf-admin/lightconf-admin-web/src/main/java/com/lightconf/admin/web/filter/CrossDomainRequestFilter.java
@@ -0,0 +1,37 @@
+package com.lightconf.admin.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 跨域请求过滤器，为每一个跨域请求添加请求头，允许任何域的请求访问服务器，存在ddos的问题。
+ * @author wuhaifei
+ * @date 2018/04/20
+ */
+public class CrossDomainRequestFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setContentType("text/html;charset=UTF-8");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        res.setHeader("Access-Control-Max-Age", "0");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("XDomainRequestAllowed","1");
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
