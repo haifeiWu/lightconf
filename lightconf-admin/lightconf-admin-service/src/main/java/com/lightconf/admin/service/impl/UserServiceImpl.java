7face1b533de2290b24e074ade7ffb7219a21de3
haifeiWu <whfstudio@163.com>
2018-04-22 15:46:26 +0800

    将用户登录更新成数据库获取数据

--- lightconf-admin/lightconf-admin-service/src/main/java/com/lightconf/admin/service/impl/UserServiceImpl.java
+++ lightconf-admin/lightconf-admin-service/src/main/java/com/lightconf/admin/service/impl/UserServiceImpl.java
@@ -1,9 +1,16 @@
 package com.lightconf.admin.service.impl;
 
+import com.lightconf.admin.dal.dao.UserMapper;
+import com.lightconf.admin.model.dataobj.User;
+import com.lightconf.admin.model.dataobj.UserExample;
 import com.lightconf.admin.service.UserService;
+import com.lightconf.common.model.Messages;
 import com.lightconf.common.util.LightConfResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author wuhf
@@ -12,8 +19,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(rollbackFor = Exception.class)
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
     @Override
     public LightConfResult userLogin(String userName, String password) {
-        return null;
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andUserNameEqualTo(userName);
+        List<User> userList = userMapper.selectByExample(userExample);
+
+        if (userList != null && userList.size() > 0) {
+            User user = userList.get(0);
+            if (user.getPassword().equals(password)) {
+                return LightConfResult.build(Messages.SUCCESS_CODE,Messages.SUCCESS_MSG,userList.get(0));
+            }
+        }
+
+        return LightConfResult.build(Messages.USER_LOGIN_ERROR_CODE,Messages.USER_LOGIN_ERROR_MSG);
     }
 }
