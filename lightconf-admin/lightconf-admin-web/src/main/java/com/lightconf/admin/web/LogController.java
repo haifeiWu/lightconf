b6d4ce212e546a7ddac16fc59e77a0947e632b0a
haifeiWu <whfstudio@163.com>
2018-04-27 00:06:39 +0800

    update

---
+++ lightconf-admin/lightconf-admin-web/src/main/java/com/lightconf/admin/web/LogController.java
@@ -0,0 +1,16 @@
+package com.lightconf.admin.web;
+
+import com.lightconf.admin.web.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 登录接口。
+ * @author wuhaifei
+ * @date 2018/04/27
+ */
+@Controller
+@RequestMapping("/log")
+public class LogController extends BaseController {
+
+}
