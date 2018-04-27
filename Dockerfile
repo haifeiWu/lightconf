20171cc1f04af4148f4e7a01bd995c663af1b6a4
haifeiWu <1253242444@qq.com>
2018-04-28 00:17:25 +0800

    add docker file and start shell

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Builder container
+FROM registry.cn-hangzhou.aliyuncs.com/aliware2018/services AS builder
+
+COPY . /root/workspace/agent
+WORKDIR /root/workspace/agent
+RUN set -ex && mvn clean package
+
+
+# Runner container
+FROM registry.cn-hangzhou.aliyuncs.com/aliware2018/debian-jdk8
+
+COPY --from=builder /root/workspace/services/mesh-provider/target/mesh-provider-1.0-SNAPSHOT.jar /root/dists/mesh-provider.jar
+COPY --from=builder /root/workspace/services/mesh-consumer/target/mesh-consumer-1.0-SNAPSHOT.jar /root/dists/mesh-consumer.jar
+COPY --from=builder /root/workspace/agent/mesh-agent/target/mesh-agent-1.0-SNAPSHOT.jar /root/dists/mesh-agent.jar
+
+COPY --from=builder /usr/local/bin/docker-entrypoint.sh /usr/local/bin
+COPY start-agent.sh /usr/local/bin
+
+RUN set -ex && mkdir -p /root/logs
+
+ENTRYPOINT ["docker-entrypoint.sh"]
---
+++ start-lightconf.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ETCD_HOST=$(ip addr show docker0 | grep 'inet\b' | awk '{print $2}' | cut -d '/' -f 1)
+ETCD_PORT=2379
+ETCD_URL=http://$ETCD_HOST:$ETCD_PORT
+
+echo ETCD_URL = $ETCD_URL
+
+if [[ "$1" == "consumer" ]]; then
+  echo "Starting consumer agent..."
+  java -jar \
+       -Xms1536M \
+       -Xmx1536M \
+       -Dtype=consumer \
+       -Dserver.port=20000\
+       -Detcd.url=$ETCD_URL \
+       -Dlogs.dir=/root/logs \
+       /root/dists/mesh-agent.jar
+elif [[ "$1" == "provider-small" ]]; then
+  echo "Starting small provider agent..."
+  java -jar \
+       -Xms512M \
+       -Xmx512M \
+       -Dtype=provider \
+       -Dserver.port=30000\
+       -Ddubbo.protocol.port=20889 \
+       -Detcd.url=$ETCD_URL \
+       -Dlogs.dir=/root/logs \
+       /root/dists/mesh-agent.jar
+elif [[ "$1" == "provider-medium" ]]; then
+  echo "Starting medium provider agent..."
+  java -jar \
+       -Xms1536M \
+       -Xmx1536M \
+       -Dtype=provider \
+       -Dserver.port=30001\
+       -Ddubbo.protocol.port=20890 \
+       -Detcd.url=$ETCD_URL \
+       -Dlogs.dir=/root/logs \
+       /root/dists/mesh-agent.jar
+elif [[ "$1" == "provider-large" ]]; then
+  echo "Starting large provider agent..."
+  java -jar \
+       -Xms2560M \
+       -Xmx2560M \
+       -Dtype=provider \
+       -Dserver.port=30002\
+       -Ddubbo.protocol.port=20891 \
+       -Detcd.url=$ETCD_URL \
+       -Dlogs.dir=/root/logs \
+       /root/dists/mesh-agent.jar
+else
+  echo "Unrecognized arguments, exit."
+  exit 1
+fi
