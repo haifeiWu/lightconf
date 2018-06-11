1f252d7c5759b66c03f99a9d4f43862ee588d350
haifeiWu <1253242444@qq.com>
2018-06-11 23:52:03 +0800

    update user controller

--- lightconf-admin/lightconf-admin-service/src/main/java/com/lightconf/admin/service/UserService.java
+++ lightconf-admin/lightconf-admin-service/src/main/java/com/lightconf/admin/service/UserService.java
@@ -2,10 +2,14 @@ package com.lightconf.admin.service;
 
 import com.lightconf.common.util.LightConfResult;
 
+import java.util.Map;
+
 /**
  * @author wuhf
  * @date 2018/04/21
  */
 public interface UserService {
     LightConfResult userLogin(String userName, String password);
+
+    Map<String, Object> getUserList(int start, int length, String username, int permission);
 }
