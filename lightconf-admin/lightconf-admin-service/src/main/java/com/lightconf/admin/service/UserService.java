df02545a72eb9c1a5d105ecb9dc496e2a8318632
haifeiWu <whfstudio@163.com>
2018-06-12 21:58:21 +0800

    update

--- lightconf-admin/lightconf-admin-service/src/main/java/com/lightconf/admin/service/UserService.java
+++ lightconf-admin/lightconf-admin-service/src/main/java/com/lightconf/admin/service/UserService.java
@@ -1,6 +1,8 @@
 package com.lightconf.admin.service;
 
+import com.lightconf.admin.model.dataobj.User;
 import com.lightconf.common.util.LightConfResult;
+import com.lightconf.common.util.ResultCode;
 
 import java.util.Map;
 
@@ -12,4 +14,10 @@ public interface UserService {
     LightConfResult userLogin(String userName, String password);
 
     Map<String, Object> getUserList(int start, int length, String username, int permission);
+
+    ResultCode<User> addUser(User confUser);
+
+    ResultCode<User> deleteUser(String username);
+
+    ResultCode<User> updateUser(User confUser);
 }
