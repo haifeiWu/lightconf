fed3a583606ce16ee7a1d8b47ffbcc4c1475719b
haifeiWu <whfstudio@163.com>
2019-04-17 11:15:44 +0800

    update

--- lightconf-admin/lightconf-admin-web/src/main/java/com/lightconf/admin/web/LightConfApplication.java
+++ lightconf-admin/lightconf-admin-web/src/main/java/com/lightconf/admin/web/LightConfApplication.java
@@ -1,11 +1,16 @@
 package com.lightconf.admin.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * @author wuhaifei
+ */
 @SpringBootApplication
 @PropertySource("classpath:light-conf.properties")
+@MapperScan(basePackages = "com.lightconf.admin.dal.dao")
 public class LightConfApplication {
     public static void main(String[] args) {
         SpringApplication.run(LightConfApplication.class, args);
