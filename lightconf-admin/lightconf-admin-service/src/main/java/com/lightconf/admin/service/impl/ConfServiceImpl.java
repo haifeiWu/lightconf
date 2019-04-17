5bb4c3c1d86a44bb8d3f4ab398bf25559c6c4e90
haifeiWu <whfstudio@163.com>
2019-04-17 18:30:13 +0800

    update

--- lightconf-admin/lightconf-admin-service/src/main/java/com/lightconf/admin/service/impl/ConfServiceImpl.java
+++ lightconf-admin/lightconf-admin-service/src/main/java/com/lightconf/admin/service/impl/ConfServiceImpl.java
@@ -15,7 +15,6 @@ import io.netty.channel.socket.SocketChannel;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
